0f0c889831d59af425fb7e6f42eafd2231c0d610
nkululek0 <nkululekovuyo0103@gmail.com>
2022-07-09 18:59:11 +0200

    created basic downloader class

---
+++ dist/class-modules/downloader.js
@@ -0,0 +1,18 @@
+import https from "https";
+import fs from "fs";
+export default class Downloader {
+    constructor(url, filename) {
+        this.url = url;
+        this.filename = filename;
+    }
+    donwload(fileOutput) {
+        https.get(this.url, (response) => {
+            let fileStream = fs.createWriteStream(`${this.filename}`);
+            response.pipe(fileStream);
+            fileStream.on("finish", () => {
+                fileStream.close();
+                console.log("donwload complete");
+            });
+        });
+    }
+}
