4ac58f5a0b9b3137d872fe5245ec3c715631a874
nkululek0 <nkululekovuyo0103@gmail.com>
2022-07-10 18:24:05 +0200

    added log functionality so user can see download progress

--- dist/class-modules/downloader.js
+++ dist/class-modules/downloader.js
@@ -16,11 +16,8 @@ export default class Downloader {
             });
             response.on("end", () => {
                 fileStream.close();
-                console.log(`${consoleOutput}: complete`);
+                console.log(` - ${consoleOutput}: complete`);
             });
         });
     }
 }
-const url = "https://d3.s2dmax.xyz/a/t4/dexters-laboratory/s04e12.mp4?tok=6F59635333567931376B7852377344444F62413662754F643856637A525738573757425A2533446C563646363349546F66364C35645644557366425667753656794E72326B532D3649634F63374B4C376853446E4C675A5A636E6F57437847685653253344744576525A734A497052327A682533444D51574D5735567968376C684A366E6E6B437176654638433539586325334450585A4E49&valid=JHCHEscaet9Nze3IQ15tqg&t=1657475987";
-const downloader = new Downloader(url, "se4-ep12-the-lab-of-tomorrow");
-downloader.donwload("Dexter, Season 4, Episode 12: the-lab-of-tomorrow");
