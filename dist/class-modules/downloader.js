957f9c115ea3b851871d75e1e5a6d8499de7be16
nkululek0 <nkululekovuyo0103@gmail.com>
2022-07-10 18:45:37 +0200

    removed async call to https function

--- dist/class-modules/downloader.js
+++ dist/class-modules/downloader.js
@@ -1,12 +1,3 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 import https from "https";
 import fs, { existsSync, mkdirSync } from "fs";
 import logUpdate from "log-update";
@@ -16,7 +7,7 @@ export default class Downloader {
         this.filename = filename;
     }
     donwload(consoleOutput) {
-        https.get(this.url, (response) => __awaiter(this, void 0, void 0, function* () {
+        https.get(this.url, (response) => {
             if (!existsSync("./downloads")) {
                 mkdirSync("./downloads");
             }
@@ -35,9 +26,6 @@ export default class Downloader {
             else {
                 console.log("file already exists!");
             }
-        }));
+        });
     }
 }
-const url = "https://d3.s2dmax.xyz/a/t4/dexters-laboratory/s04e12.mp4?tok=6F59635333567931376B7852377344444F62413662754F643856637A525738573757425A2533446C563646363349546F66364C35645644557366425667753656794E72326B532D3649634F63374B4C376853446E4C675A5A636E6F57437847685653253344744576525A734A497052327A682533444D51574D5735567968376C684A366E6E6B437176654638433539586325334450585A4E49&valid=JHCHEscaet9Nze3IQ15tqg&t=1657475987";
-const downloader = new Downloader(url, "se4-ep12-the-lab-of-tomorrow.mp4");
-downloader.donwload("Dexter, Season 4, Episode 12: the-lab-of-tomorrow");
