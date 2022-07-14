a9fc30156d0bfd3f9c5cb194a370d75de7d4ea38
nkululek0 <nkululekovuyo0103@gmail.com>
2022-07-14 14:25:33 +0200

    completed movie selection and download functionality

--- dist/index.js
+++ dist/index.js
@@ -12,6 +12,7 @@ import stealthPlugin from "puppeteer-extra-plugin-stealth";
 import readline from "readline";
 import Movie from "./class-modules/movie.js";
 import Series from "./class-modules/series.js";
+import Downloader from "./class-modules/downloader.js";
 puppeteer.use(stealthPlugin());
 (() => __awaiter(void 0, void 0, void 0, function* () {
     let tabContent = [];
@@ -25,7 +26,7 @@ puppeteer.use(stealthPlugin());
     })(options || (options = {}));
     ;
     let userInput = 0;
-    consolePrompt.question("1.Movie\n2.Series\nselect option: ", (answer) => {
+    consolePrompt.question("1.Movie\n2.Series\nSelect Option: ", (answer) => {
         if (answer === "1") {
             userInput = options.movie;
         }
@@ -35,11 +36,11 @@ puppeteer.use(stealthPlugin());
         else {
             console.log("invalid input");
         }
-        consolePrompt.question("search: ", (answer) => __awaiter(void 0, void 0, void 0, function* () {
-            const browser = yield puppeteer.launch({ headless: false });
+        consolePrompt.question("Search: ", (answer) => __awaiter(void 0, void 0, void 0, function* () {
+            const browser = yield puppeteer.launch();
             const tab = yield browser.newPage();
             yield tab.goto("https://soap2day.to/enter.html");
-            yield tab.waitForTimeout(3000);
+            yield tab.waitForTimeout(1500);
             yield tab.click("#btnhome");
             yield tab.waitForNavigation();
             if (userInput === options.movie) {
@@ -50,9 +51,28 @@ puppeteer.use(stealthPlugin());
             }
             yield search(mainObject.title, tab);
             yield setTabContent(mainObject.type, tab);
-            console.log(tabContent);
+            yield tab.waitForTimeout(1000).then(() => {
+                console.log("Donwload Options:");
+                tabContent.map((item, index) => {
+                    console.log(`${++index}. ${item.name}`);
+                });
+                consolePrompt.question("Select Option: ", (answer) => __awaiter(void 0, void 0, void 0, function* () {
+                    let numAnswer = Number(answer) - 1;
+                    if (userInput === options.movie) {
+                        yield tab.goto(tabContent[numAnswer].url);
+                        yield tab.waitForSelector("video");
+                        let downLoadSrc = yield tab.$eval("video", (video) => {
+                            return video.src;
+                        });
+                        const downloader = new Downloader(downLoadSrc, `${tabContent[numAnswer].name}.mp4`);
+                        downloader.donwload(tabContent[numAnswer].name);
+                        yield browser.close();
+                    }
+                }));
+            });
         }));
     });
+    // searches user console input
     function search(searchInput, tab) {
         return __awaiter(this, void 0, void 0, function* () {
             yield tab.type("#txtSearch", searchInput);
@@ -60,6 +80,7 @@ puppeteer.use(stealthPlugin());
             yield tab.waitForSelector(".no-padding");
         });
     }
+    // populates tabContent based on the content of the current page and type
     function setTabContent(type, tab) {
         return __awaiter(this, void 0, void 0, function* () {
             const typeSection = yield tab.$$(".panel-body");
@@ -85,9 +106,7 @@ puppeteer.use(stealthPlugin());
             }
         });
     }
-    function getTabContent() {
-        return tabContent;
-    }
+    // does the formating for setContent so that setContent is DRY
     function formatTabContent(match, selector) {
         return __awaiter(this, void 0, void 0, function* () {
             if (match === "no match") {
